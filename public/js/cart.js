ed9c652343558f5ba79213677a835cf8d31c7a3a
Manh Trong Kien <manhtrongkien1901@gmail.com>
2024-12-22 00:31:03 +0700

    Fix cart.js to make sure that when the 'ajax product' is added, the 'add-to-cart button' will work correctly.

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".add-to-cart-btn");
+  const productContainer = document.getElementById('product-list'); 
   const userId = document.getElementById("user-id").value;
   console.log("User ID:", userId);
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
-      if(userId==""){
+  productContainer.addEventListener("click", async (event) => {
+    if (event.target && event.target.matches(".add-to-cart-btn")) {
+      if (userId == "") {
         window.location.href = "/login"; 
-      return;
+        return;
       }
       event.preventDefault();
-      const productId = button.getAttribute("data-product-id");
+      const productId = event.target.getAttribute("data-product-id");
+      console.log(productId);
       const quantityElement = document.getElementById('quantity');
       const quantity = quantityElement ? quantityElement.innerText : 1;
-      const price = button.getAttribute("data-product-price");
+      const price = event.target.getAttribute("data-product-price");
+
       try {
         const response = await fetch("/cart/add", {
           method: "POST",
@@ -29,14 +31,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         const result = await response.json();
         if (response.ok) {
-          alert(result.message); // Success message
+          alert(result.message); 
         } else {
-          alert(result.message); // Use backticks for template strings
+          alert(result.message); 
         }
       } catch (error) {
         console.error("Error adding product to cart:", error);
         alert("Error adding product to cart");
       }
-    });
+    }
   });
 });
